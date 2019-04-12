3a29fd4c30618abe6fa7ad357d51f8aa24585ef2
unknown <lappjeff20@gmail.com>
2019-04-12 11:29:16 -0400

    Finished getData action creator

--- smurfs/src/actions/getData.js
+++ smurfs/src/actions/getData.js
@@ -1,7 +1,18 @@
+import axios from 'axios'
+
 export const GET_DATA_START = 'GET_DATA_START'
 export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS'
 export const GET_DATA_FAIL = 'GET_DATA_FAIL'
 
 export const getData = () => dispatch => {
-  dispatch({})
+  dispatch({ type: GET_DATA_START })
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: GET_DATA_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: GET_DATA_FAIL , payload: err })
+    })
 }
